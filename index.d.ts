0ad94b775881a5558ac480153bd451862c203d0b
deda <boyan.dedic@gmail.com>
2020-08-04 03:30:39 +0200

    typescript final

--- index.d.ts
+++ index.d.ts
@@ -1,32 +1,26 @@
 import { ObjectSchema } from "@hapi/joi";
 import { RequestHandler } from "express";
 
-export default {
-  /**
-   * Checks validation for the body params.
-   *
-   * @param {ObjectSchema} schema Joi schema.
-   * @returns {void} The resulting sum of the two numbers.
-   */
-  bodyValidation: (
-    schema: ObjectSchema
-  ): RequestHandler<P, ResBody, ReqBody, ReqQuery> => {},
-  /**
-   * Checks validation for the express route params ex. /profile/:id
-   *
-   * @param {ObjectSchema} schema Joi schema.
-   * @returns {void} The resulting sum of the two numbers.
-   */
-  paramsValidation: (
-    schema: ObjectSchema
-  ): RequestHandler<P, ResBody, ReqBody, ReqQuery> => {},
-  /**
-   * Checks validation for the URL query params
-   *
-   * @param {ObjectSchema} schema Joi schema.
-   * @returns {void} The resulting sum of the two numbers.
-   */
-  queryValidation: (
-    schema: ObjectSchema
-  ): RequestHandler<P, ResBody, ReqBody, ReqQuery> => {},
-};
+/**
+ * Checks validation for the body params.
+ *
+ * @param {ObjectSchema} schema Joi schema.
+ * @returns {RequestHandler} Request handler.
+ */
+export function bodyValidation(schema: ObjectSchema): RequestHandler;
+
+/**
+ * Checks validation for the express params ex. /profile/:id.
+ *
+ * @param {ObjectSchema} schema Joi schema.
+ * @returns {RequestHandler} Request handler.
+ */
+export function paramsValidation(schema: ObjectSchema): RequestHandler;
+
+/**
+ * Checks validation for the URL params.
+ *
+ * @param {ObjectSchema} schema Joi schema.
+ * @returns {RequestHandler} Request handler.
+ */
+export function queryValidation(schema: ObjectSchema): RequestHandler;
